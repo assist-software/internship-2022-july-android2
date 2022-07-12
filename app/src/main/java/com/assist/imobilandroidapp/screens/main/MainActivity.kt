c963d9c53d46ca0aad6f01c16f48985da8df4077
EusebiuCandrea <candrea.eusebiu95@gamil.com>
2022-07-12 11:33:26 +0300

    Init project

---
+++ app/src/main/java/com/assist/imobilandroidapp/screens/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.assist.imobilandroidapp.screens.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.assist.imobilandroidapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
