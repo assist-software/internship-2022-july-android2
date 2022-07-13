865f713c0248c121583ea084d06e4d41e3b1ea92
agosh <agageldidurdyyev@gmail.com>
2022-07-13 14:11:28 +0300

    sign up screen validations

---
+++ app/src/main/java/Validations.kt
@@ -0,0 +1,45 @@
+import android.util.Patterns
+import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
+
+class Validations {
+    companion object {
+
+        fun validEmail(emailText: TextInputEditText): String? {
+
+
+            val emailTextValue = emailText.text.toString();
+            if (!Patterns.EMAIL_ADDRESS.matcher(emailTextValue).matches()) {
+                return "Invalid email address"
+            }
+            return null
+        }
+
+        fun validPassword(passwordEditText: TextInputEditText): String? {
+
+            val passwordText = passwordEditText.text.toString();
+
+            if (passwordText.length < 8) {
+                return "At least 8 characters and one number."
+            }
+
+//        if(!passwordText.matches(".*[A-Z]*.".toRegex()))
+//        {
+//            return "Must contain 1 uppercase caracter"
+//        }
+
+//        if(!passwordText.matches(".*[a-z]*.".toRegex()))
+//        {
+//            return "Must contain 1 lowercase caracter"
+//        }
+
+//        if(!passwordText.matches(".*[@#\$%^&+=]*.".toRegex()))
+//        {
+//            return "Must contain 1 special caracter"
+//        }
+            return null
+        }
+
+
+    }
+}
--- app/src/main/java/com/assist/imobilandroidapp/screens/signup/SignUp.kt
+++ app/src/main/java/com/assist/imobilandroidapp/screens/signup/SignUp.kt
@@ -2,11 +2,44 @@ package com.assist.imobilandroidapp.screens.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import com.assist.imobilandroidapp.R
+import com.assist.imobilandroidapp.databinding.ActivitySignUpBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var emailEditTextView: TextInputEditText
+    private lateinit var passwordEditTextView: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        emailFocusListener()
+        passwordFocusListener()
+        emailEditTextView = findViewById(R.id.emailEditText)
+        passwordEditTextView = findViewById(R.id.passwordEditText)
+
     }
+
+    private fun emailFocusListener() {
+        binding.emailEditText.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.emailContainer.helperText = Validations.validEmail(emailEditTextView)
+            }
+        }
+    }
+
+    private fun passwordFocusListener() {
+        binding.passwordEditText.setOnFocusChangeListener { _, focused ->
+            if (!focused) {
+                binding.passwordContainer.helperText =
+                    Validations.validPassword(passwordEditTextView)
+            }
+        }
+    }
+
+
 }
