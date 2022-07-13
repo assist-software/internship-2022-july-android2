d6c1a553de66d57d92492fd053abdea31ffd75cb
agosh <agageldidurdyyev@gmail.com>
2022-07-13 16:29:24 +0300

    signUp validation v2

--- app/src/main/java/com/assist/imobilandroidapp/screens/login/Login.kt
+++ app/src/main/java/com/assist/imobilandroidapp/screens/login/Login.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.assist.imobilandroidapp.R
+import com.assist.imobilandroidapp.screens.forgotPassword.ForgotPassword
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +20,7 @@ class Login : AppCompatActivity() {
         val intent = Intent(this, ForgotPassword::class.java)
         startActivity(intent)
     }
+
 
     fun goToSignUp(view: View ){
         val intent = Intent(this, SignUp::class.java)
