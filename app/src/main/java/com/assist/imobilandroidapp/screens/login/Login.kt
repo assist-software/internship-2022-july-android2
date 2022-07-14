776ebab171e0c7f0848cb99a5eb175f029f92cae
agosh <agageldidurdyyev@gmail.com>
2022-07-14 08:55:03 +0300

    Signup screen validation only

--- app/src/main/java/com/assist/imobilandroidapp/screens/login/Login.kt
+++ app/src/main/java/com/assist/imobilandroidapp/screens/login/Login.kt
@@ -6,28 +6,15 @@ import android.os.Bundle
 import android.view.View
 import com.assist.imobilandroidapp.R
 import com.assist.imobilandroidapp.screens.forgotPassword.ForgotPassword
-<<<<<<< HEAD
-=======
 import com.assist.imobilandroidapp.screens.main.SignUp
->>>>>>> e95ef4ec55e64c82640c90aca2ee13f776e10e20
 
 class Login : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
 
 
-
-
-    fun goToForgotPassword(view: View){
-        val intent = Intent(this, ForgotPassword::class.java)
-        startActivity(intent)
-    }
-
-
-    fun goToSignUp(view: View ){
-        val intent = Intent(this, SignUp::class.java)
-        startActivity(intent)
-    }
 }
--- app/src/main/java/com/assist/imobilandroidapp/screens/signup/SignUp.kt
+++ app/src/main/java/com/assist/imobilandroidapp/screens/signup/SignUp.kt
@@ -3,21 +3,14 @@ package com.assist.imobilandroidapp.screens.main
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Patterns
-=======
->>>>>>> e95ef4ec55e64c82640c90aca2ee13f776e10e20
-import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import com.assist.imobilandroidapp.R
-import com.assist.imobilandroidapp.databinding.ActivitySignUpBinding
-<<<<<<< HEAD
 import com.google.android.material.button.MaterialButton
-=======
 import com.assist.imobilandroidapp.screens.login.Login
->>>>>>> e95ef4ec55e64c82640c90aca2ee13f776e10e20
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import java.util.regex.Pattern
@@ -26,16 +19,19 @@ class SignUp : AppCompatActivity() {
 
     private lateinit var emailEditTextView: TextInputEditText
     private lateinit var passwordEditTextView: TextInputEditText
-    private lateinit var signupButton : MaterialButton
-    private lateinit var emailContainerLayout : TextInputLayout
-    private lateinit var passwordContainerLayout : TextInputLayout
+    private lateinit var signupButton: MaterialButton
+    private lateinit var emailContainerLayout: TextInputLayout
+    private lateinit var passwordContainerLayout: TextInputLayout
+    private lateinit var logInTextView : TextView
 
 
-    private val passwordPattern : Pattern = Pattern.compile("^" +
-            "(?=.*[!?@#$%^&+=])" +     // at least 1 special character
-            "(?=\\S+$)" +            // no white spaces
-            ".{8,}" +                // at least 8 characters
-            "$")
+    private val passwordPattern: Pattern = Pattern.compile(
+        "^" +
+                "(?=.*[!?@#$%^&+=])" +     // at least 1 special character
+                "(?=\\S+$)" +            // no white spaces
+                ".{8,}" +                // at least 8 characters
+                "$"
+    )
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,23 +44,22 @@ class SignUp : AppCompatActivity() {
         signupButton = findViewById(R.id.signUpButton)
         emailContainerLayout = findViewById(R.id.emailContainer)
         passwordContainerLayout = findViewById(R.id.passwordContainer)
+        logInTextView = findViewById(R.id.logInTextView)
 
 
-        emailContainerLayout.editText?.addTextChangedListener(object: TextWatcher{
+        emailContainerLayout.editText?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 val emailTextValue = emailContainerLayout.editText!!.text.toString()
 
-                if(emailTextValue.isEmpty()){
+                if (emailTextValue.isEmpty()) {
                     emailContainerLayout.error = "Email is required"
-                }
-                else{
-                    if(!Patterns.EMAIL_ADDRESS.matcher(emailTextValue).matches()) {
+                } else {
+                    if (!Patterns.EMAIL_ADDRESS.matcher(emailTextValue).matches()) {
                         emailContainerLayout.error = "Invalid email address"
-                    }
-                    else {
+                    } else {
                         emailContainerLayout.error = ""
                     }
                 }
@@ -75,22 +70,20 @@ class SignUp : AppCompatActivity() {
         })
 
 
-        passwordContainerLayout.editText?.addTextChangedListener(object: TextWatcher {
+        passwordContainerLayout.editText?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                val passwordValue  = passwordContainerLayout.editText!!.text.toString()
+                val passwordValue = passwordContainerLayout.editText!!.text.toString()
 
-                if(passwordValue.isEmpty()){
+                if (passwordValue.isEmpty()) {
                     passwordContainerLayout.error = "Password is required"
-                }
-
-                else {
-                    if(!passwordPattern.matcher(passwordValue).matches()){
-                        passwordContainerLayout.error = "Password needs to contain at least 8 characters and one number."
-                    }
-                    else {
+                } else {
+                    if (!passwordPattern.matcher(passwordValue).matches()) {
+                        passwordContainerLayout.error =
+                            "Password needs to contain at least 8 characters and one number."
+                    } else {
                         passwordContainerLayout.error = ""
                     }
                 }
@@ -99,16 +92,30 @@ class SignUp : AppCompatActivity() {
             override fun afterTextChanged(p0: Editable?) {
             }
         })
-        signupButton.setOnClickListener{
-            Toast.makeText(this, "Yaaay!", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this,MainActivity::class.java))
+        signupButton.setOnClickListener {
+
+            if (emptyInputValidation(emailContainerLayout) && emptyInputValidation(passwordContainerLayout)) {
+                Toast.makeText(this, "Yaaay!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        logInTextView.setOnClickListener{
+            startActivity(Intent(this, Login::class.java))
             finish()
         }
 
     }
 
+    private fun emptyInputValidation(input: TextInputLayout): Boolean {
+        val value  = input.editText?.text!!
 
+        if(value.isEmpty()){
+            input.error = "Please fill in the field."
+            return false
+        }
 
+        return true
+    }
 
 
 }
